fa5f041b0c12c5fd2c43f46cf2582e986b42a11f
Andrei Karpenko <7498329@gmail.com>
2023-05-12 14:20:48 -0700

    add sounds for a right and wrong answer

--- scripts/testing.js
+++ scripts/testing.js
@@ -1,3 +1,5 @@
+const audioCorrect = new Audio("/assets/audio.wav");
+const audioWrong = new Audio("/assets/audioWrong.mp3");
 const submitButton = document.getElementById("submit");
 const questionContainer = document.querySelector(".question-container");
 // const questionContainerSub = document.querySelector(".result-container");
@@ -6,8 +8,10 @@ submitButton.addEventListener("click", () => {
   const selectedAnswer = document.querySelector('input[name="answer"]:checked');
   if (selectedAnswer && selectedAnswer.value === "c") {
     questionContainer.style.backgroundColor = "rgba(59, 234, 59, 0.5)";
+    audioCorrect.play();
     // document.body.style.backgroundImage = "url('/assets/right.jpg')";
   } else {
+    audioWrong.play();
     questionContainer.style.backgroundColor = "rgba(211, 47, 47, 0.5)";
     // document.body.style.backgroundImage = "url('/assets/wrong.jpg')";
   }
